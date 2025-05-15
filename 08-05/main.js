f74708a12b6608ed5beca3a0de8c5feddd8ecd97
Isis rafae <161063552+sucodemorang@users.noreply.github.com>
2025-05-15 15:43:16 -0300

    Add files via upload

---
+++ 08-05/main.js
@@ -0,0 +1,18 @@
+function adicionarTarefa() {
+    const input = document.getElementById('nova-tarefa');
+    const textoTarefa = input.value.trim();
+
+    if (textoTarefa === '') return;
+
+    const li = document.createElement('li');
+    li.textContent = textoTarefa;
+
+    const botaoRemover = document.createElement('button');
+    botaoRemover.textContent = 'Remover';
+    botaoRemover.onclick = () => li.remove();
+
+    li.appendChild(botaoRemover);
+    document.getElementById('listaTarefa').appendChild(li);
+
+    input.value = '';
+}
